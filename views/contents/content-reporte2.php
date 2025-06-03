c47ef8655e718ff99e3c03baff22ab537a958dca
Mariana Apolaya <apolayaschradermariana@gmail.com>
2025-06-03 16:39:24 -0500

    Mejora del reporte 2

--- views/contents/content-reporte2.php
+++ views/contents/content-reporte2.php
@@ -6,11 +6,15 @@
   <title>Document</title>
 </head>
 <body>
-  <h1>SENATI</h1>
-  <h2 class="importante">Reporte 2</h2>
-  <p>Este es un ejemplo de reporte generado con la librería Html2Pdf.</p>
-  <p>La librería Html2Pdf permite convertir HTML a PDF de manera sencilla.</p>  
-  <p>Este reporte incluye texto, imágenes y estilos básicos.</p>
-  <p>Puede personalizar el contenido y el estilo según sus necesidades.</p> 
+<h1>Veterinaria “Mi Mascota”</h1>
+<h2>Reporte de Mascotas</h2>
+
+<p class="descripcion">
+  Este es un reporte de ejemplo generado con la librería <strong>Html2Pdf</strong>. 
+  A continuación, se presenta una tabla ficticia con datos sobre mascotas y sus propietarios.
+</p>
+
+
+
 </body>
 </html>
